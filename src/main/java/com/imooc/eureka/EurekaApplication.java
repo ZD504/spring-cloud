2d99c63f322f73275078216846d59351228b4486
JD <935157331@qq.com>
2018-09-19 17:42:57 +0800

    Changed the content of main app.
    Signed-off-by: JD <935157331@qq.com>

--- src/main/java/com/imooc/eureka/EurekaApplication.java
+++ src/main/java/com/imooc/eureka/EurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("The eureka server has started.");
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
